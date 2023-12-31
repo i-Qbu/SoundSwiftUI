45fd18382989839eca8a369c669329c9b215ff41
i-qbu <iqbu.lab@gmail.com>
2023-12-31 09:54:23 +0900

    Initial Commit

---
+++ Sound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sound
+//
+//  Created by 渡辺大智 on 2023/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sound/SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundApp.swift
+//  Sound
+//
+//  Created by 渡辺大智 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
